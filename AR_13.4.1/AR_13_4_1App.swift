4be5d673ba073c33f45f4597698e825f3f796cf9
David <ber.dev@yandex.ru>
2023-01-08 01:55:31 +0300

    Initial Commit

---
+++ AR_13.4.1/AR_13_4_1App.swift
@@ -0,0 +1,17 @@
+//
+//  AR_13_4_1App.swift
+//  AR_13.4.1
+//
+//  Created by Дэвид Бердников on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AR_13_4_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR_13.4.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AR_13.4.1
+//
+//  Created by Дэвид Бердников on 08.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
